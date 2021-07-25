386a0858dc22ed74d0a724340dc22a08785fb5d3
Tariqkumraj <86144917+Tariqkumraj@users.noreply.github.com>
2021-07-25 22:58:08 +0500

    Add files via upload

---
+++ index.server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const env = require('dotenv');
+const app = express();
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+
+//routes
+const userRoutes = require('./routes/user');
+const { json } = require('body-parser');
+
+//environment variable or constant
+env.config();
+
+//mongoDb connection
+//mongodb+srv://admin:<password>@cluster0.uv3jg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+ mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.uv3jg.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+    ).then(() => {
+        console.log('Database Connected');
+    });
+    app.use(bodyParser.json());
+    
+    //bodyparser is updated:
+    //app.use(bodyParser.urlencoded({ extended: true }));
+    app.use('/api', userRoutes);
+
+app.get('/', (req, res, next) => {
+  res.status(200).json({
+      message: 'hello from server'      
+  });
+});
+
+app.post('/data', (req, res, next) => {
+    res.status(200).json({
+        message: req.body
+    });
+  });
+  
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+
